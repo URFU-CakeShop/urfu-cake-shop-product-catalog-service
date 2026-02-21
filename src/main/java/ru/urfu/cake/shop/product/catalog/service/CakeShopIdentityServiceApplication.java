4b4dd3d5f81d37415dbec60f16de7ee4b82ec958
Mihail Vorobyev <1viklover@gmail.com>
2026-02-21 12:04:24 +0500

    Init spring boot application

---
+++ src/main/java/ru/urfu/cake/shop/product/catalog/service/CakeShopIdentityServiceApplication.java
@@ -0,0 +1,12 @@
+package ru.urfu.cake.shop.product.catalog.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeShopIdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeShopIdentityServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/urfu/cake/shop/product/catalog/service/CakeShopProductCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.urfu.cake.shop.product.catalog.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CakeShopIdentityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

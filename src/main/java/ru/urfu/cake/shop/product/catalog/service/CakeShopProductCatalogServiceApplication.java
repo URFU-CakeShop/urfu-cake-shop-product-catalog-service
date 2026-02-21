5046cc2bd6199680e6bab6659feef10ddd900aa4
Mihail Vorobyev <1viklover@gmail.com>
2026-02-21 12:08:11 +0500

    Minor fixes

--- src/main/java/ru/urfu/cake/shop/product/catalog/service/CakeShopProductCatalogServiceApplication.java
+++ src/main/java/ru/urfu/cake/shop/product/catalog/service/CakeShopProductCatalogServiceApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CakeShopIdentityServiceApplication {
+public class CakeShopProductCatalogServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CakeShopIdentityServiceApplication.class, args);
+		SpringApplication.run(CakeShopProductCatalogServiceApplication.class, args);
 	}
 }

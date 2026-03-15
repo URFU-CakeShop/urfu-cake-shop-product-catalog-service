845f69c035c81d03473e0596cdd3d87d50da2534
Mihail Vorobyev <1viklover@gmail.com>
2026-03-15 22:41:27 +0500

    Add endpoints to product types

--- src/main/java/ru/urfu/cake/shop/product/catalog/service/controller/ProductController.java
+++ src/main/java/ru/urfu/cake/shop/product/catalog/service/controller/ProductController.java
@@ -5,17 +5,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.urfu.cake.shop.product.catalog.service.dto.request.CreateProductCategoryDto;
+import ru.urfu.cake.shop.product.catalog.service.dto.request.CreateProductTypeDto;
 import ru.urfu.cake.shop.product.catalog.service.dto.response.ApiResponse;
 import ru.urfu.cake.shop.product.catalog.service.dto.request.CreateProductDto;
 import ru.urfu.cake.shop.product.catalog.service.dto.response.ProductCategoryDto;
 import ru.urfu.cake.shop.product.catalog.service.dto.response.ProductDto;
 import ru.urfu.cake.shop.product.catalog.service.dto.response.ProductTypeDto;
+import ru.urfu.cake.shop.product.catalog.service.entity.ProductType;
 import ru.urfu.cake.shop.product.catalog.service.exception.ProductCategoryNotFoundException;
 import ru.urfu.cake.shop.product.catalog.service.exception.ProductTypeNotFoundException;
 import ru.urfu.cake.shop.product.catalog.service.repository.ProductCategoryRepository;
 import ru.urfu.cake.shop.product.catalog.service.repository.ProductTypeRepository;
 import ru.urfu.cake.shop.product.catalog.service.service.ProductService;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.UUID;
 /**
  * Контроллер продуктов
@@ -44,13 +48,42 @@ public class ProductController extends BaseController {
         }
     }
     /**
+     * Создать тип продукта
+     *
+     * @param request Запрос
+     * @return Результат обработки запроса
+     */
+    @PostMapping("product/type")
+    public ResponseEntity<ApiResponse<ProductTypeDto>> createProductType(@RequestBody CreateProductTypeDto request) {
+        var productType = new ProductType();
+        productType.setName(request.getName());
+        var productTypeSaved = productTypeRepository.save(productType);
+        var result = toDto(productTypeSaved);
+        return buildSuccessResponse(result);
+    }
+    /**
+     * Получить все возможные типы
+     *
+     * @return Результат обработки запроса
+     */
+    @GetMapping("product/type")
+    public ResponseEntity<ApiResponse<List<ProductTypeDto>>> createProductType() {
+        var productTypeList = productTypeRepository.findAll();
+        var productTypeDtoList = new LinkedList<ProductTypeDto>();
+        for (var productType : productTypeList) {
+            var productTypeDto = toDto(productType);
+            productTypeDtoList.addLast(productTypeDto);
+        }
+        return buildSuccessResponse(productTypeDtoList);
+    }
+    /**
      * Получить тип продукта
      *
      * @param typeId Идентификатор типа продукта
      * @return Тип продукта
      */
     @GetMapping("product/type/{id}")
-    public ResponseEntity<ApiResponse<ProductTypeDto>> getProductType(@PathVariable UUID typeId) {
+    public ResponseEntity<ApiResponse<ProductTypeDto>> getProductType(@PathVariable(name = "id") UUID typeId) {
         var typeOptional = productTypeRepository.findById(typeId);
         if (typeOptional.isEmpty()) {
             return buildFailResponse(HttpStatus.NOT_FOUND, "Product type not found");
--- src/main/java/ru/urfu/cake/shop/product/catalog/service/entity/ProductType.java
+++ src/main/java/ru/urfu/cake/shop/product/catalog/service/entity/ProductType.java
@@ -1,9 +1,11 @@
 package ru.urfu.cake.shop.product.catalog.service.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
 /**
@@ -14,6 +16,8 @@ import java.util.UUID;
 @Data
 public class ProductType {
     @Id
+    @GeneratedValue
+    @UuidGenerator
     private UUID id;
     private String name;
 }
